cb6a034e71de8b496597a33b098e4fa9a114b85a
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2023-04-04 21:56:24 -0500

    proveedores

--- src/main/java/com/example/lab2/controller/ProveedoresController.java
+++ src/main/java/com/example/lab2/controller/ProveedoresController.java
@@ -1,10 +1,29 @@
 package com.example.lab2.controller;
 
+import com.example.lab2.entity.Proveedores;
+import com.example.lab2.repository.ProveedoresRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping ("Proveedores")
+@RequestMapping ("/Proveedores")
 public class ProveedoresController {
+
+    final ProveedoresRepository proveedoresRepository;
+
+    public ProveedoresController (ProveedoresRepository proveedoresRepository){
+        this.proveedoresRepository =  proveedoresRepository;
+    }
+
+    @GetMapping ("/listar")
+    public String listarProveedores(){
+        List<Proveedores> lis = proveedoresRepository.findAll();
+            for (Proveedores p : lis){
+                System.out.printf("id proveedores: "+ p.getrepo);
+            }
+    }
 
 }

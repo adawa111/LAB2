0dd5663de962acb90733460eb227f4da8d1ef3a4
angie2512 <a20191792@pucp.edu.pe>
2023-04-04 22:01:05 -0500

    htmls

--- src/main/java/com/example/lab2/entity/Artistas.java
+++ src/main/java/com/example/lab2/entity/Artistas.java
@@ -1,4 +1,68 @@
 package com.example.lab2.entity;
 
+import jakarta.persistence.*;
+
+import java.sql.Date;
+import java.util.Objects;
+
+@Entity
 public class Artistas {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "idArtistas")
+    private int idArtistas;
+    @Basic
+    @Column(name = "Nombre_Grupo")
+    private String nombreGrupo;
+    @Basic
+    @Column(name = "Fecha_creacion")
+    private Date fechaCreacion;
+    @Basic
+    @Column(name = "Tipo_musica")
+    private String tipoMusica;
+
+    public int getIdArtistas() {
+        return idArtistas;
+    }
+
+    public void setIdArtistas(int idArtistas) {
+        this.idArtistas = idArtistas;
+    }
+
+    public String getNombreGrupo() {
+        return nombreGrupo;
+    }
+
+    public void setNombreGrupo(String nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getTipoMusica() {
+        return tipoMusica;
+    }
+
+    public void setTipoMusica(String tipoMusica) {
+        this.tipoMusica = tipoMusica;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artistas artistas = (Artistas) o;
+        return idArtistas == artistas.idArtistas && Objects.equals(nombreGrupo, artistas.nombreGrupo) && Objects.equals(fechaCreacion, artistas.fechaCreacion) && Objects.equals(tipoMusica, artistas.tipoMusica);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idArtistas, nombreGrupo, fechaCreacion, tipoMusica);
+    }
 }
--- src/main/java/com/example/lab2/entity/Integrantes.java
+++ src/main/java/com/example/lab2/entity/Integrantes.java
@@ -1,4 +1,89 @@
 package com.example.lab2.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
 public class Integrantes {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "idIntegrantes")
+    private int idIntegrantes;
+    @Basic
+    @Column(name = "Nombre")
+    private String nombre;
+    @Basic
+    @Column(name = "Edad")
+    private Integer edad;
+    @Basic
+    @Column(name = "Anhios_exp")
+    private Integer anhiosExp;
+    @Basic
+    @Column(name = "rol")
+    private String rol;
+    @Basic
+    @Column(name = "artistas_idArtistas")
+    private int artistasIdArtistas;
+
+    public int getIdIntegrantes() {
+        return idIntegrantes;
+    }
+
+    public void setIdIntegrantes(int idIntegrantes) {
+        this.idIntegrantes = idIntegrantes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Integer getAnhiosExp() {
+        return anhiosExp;
+    }
+
+    public void setAnhiosExp(Integer anhiosExp) {
+        this.anhiosExp = anhiosExp;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getArtistasIdArtistas() {
+        return artistasIdArtistas;
+    }
+
+    public void setArtistasIdArtistas(int artistasIdArtistas) {
+        this.artistasIdArtistas = artistasIdArtistas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Integrantes that = (Integrantes) o;
+        return idIntegrantes == that.idIntegrantes && artistasIdArtistas == that.artistasIdArtistas && Objects.equals(nombre, that.nombre) && Objects.equals(edad, that.edad) && Objects.equals(anhiosExp, that.anhiosExp) && Objects.equals(rol, that.rol);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idIntegrantes, nombre, edad, anhiosExp, rol, artistasIdArtistas);
+    }
 }

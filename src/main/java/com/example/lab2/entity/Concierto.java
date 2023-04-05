68c51261afdda04423005da50680803c703278c7
Abigail Ramirez <abigail.ramirez@pucp.edu.pe>
2023-04-04 22:01:49 -0500

    abi

--- src/main/java/com/example/lab2/entity/Concierto.java
+++ src/main/java/com/example/lab2/entity/Concierto.java
@@ -1,4 +1,45 @@
 package com.example.lab2.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "concierto")
 public class Concierto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idConcierto")
+    private int id;
+    @Column(name = "Fecha_evento")
+    private String fechaEvento;
+    @Column(name = "artistas_idArtistas")
+    private int artistasId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFechaEvento() {
+        return fechaEvento;
+    }
+
+    public void setFechaEvento(String fechaEvento) {
+        this.fechaEvento = fechaEvento;
+    }
+
+    public int getArtistasId() {
+        return artistasId;
+    }
+
+    public void setArtistasId(int artistasId) {
+        this.artistasId = artistasId;
+    }
 }
--- src/main/java/com/example/lab2/repository/ConciertoRepository.java
+++ src/main/java/com/example/lab2/repository/ConciertoRepository.java
@@ -1,4 +1,15 @@
 package com.example.lab2.repository;
 
-public class ConciertoRepository {
+import com.example.lab2.entity.Concierto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ConciertoRepository
+        extends JpaRepository<Concierto, Integer> {
+
+
+
 }
